816ef415eb0401c65e82ff48184d66e66315e4ef
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-04 11:38:46 -0700

    feat: add handle query button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import { handleAdd, handleQueryDelete } from '../../utils/utils'
 
 // styles
 import styles from './Header.module.css'
 
 const Header = () => {
-  const handleAdd = () => {
-    console.log('handleAdd clicked.');
-  }
+
   return (
     <div className={styles.headerWrap}>
         <h1 className={styles.headerTitle}>Colour Palette Builder</h1>
-        <button className={styles.rainbowButton} onClick={handleAdd}>Add New Colour</button>
+        <div className={styles.headerBtns}>
+          <button className={styles.rainbowButton} onClick={handleQueryDelete}>Query Delete</button>
+          <button className={styles.rainbowButton} onClick={handleAdd}>Add New Colour</button>
+        </div>
+
     </div>
   )
 }
